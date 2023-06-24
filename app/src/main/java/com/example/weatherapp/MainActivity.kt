359dff4211d08af5ee123e762ba1f781bbb5c7c6
Katie Gingell <katiegingell@Katies-MacBook-Pro.local>
2023-06-23 22:01:04 -0500

    added toolbar and weather data values

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,7 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    WeatherDataView()
                 }
             }
         }
@@ -35,6 +40,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun WeatherDataView(){
+    Column() {
+        Text(text = stringResource(id = R.string.city))
+        Row(){
+            Column() {
+                Text(text = stringResource(id = R.string.temperature))
+                Text(text = stringResource(id = R.string.feels_like_temp))
+            }
+            Text(text = "sun image")
+        }
+        Text(text = stringResource(id = R.string.low_temp))
+        Text(text = stringResource(id = R.string.high_temp))
+        Text(text = stringResource(id = R.string.humidity))
+        Text(text = stringResource(id = R.string.pressure))
+    }
 }
 
 @Preview(showBackground = true)

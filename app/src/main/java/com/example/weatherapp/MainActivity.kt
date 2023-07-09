c0543d7a80f382db04101b1e5f9207bbdb4d4486
katiegingell <133730705+katiegingell@users.noreply.github.com>
2023-07-09 12:26:40 -0500

    Assignment1 (#1)
    * added toolbar and weather data values
    
    * Assignment 1
    
    ---------
    
    Co-authored-by: Katie Gingell <katiegingell@Katies-MacBook-Pro.local>

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,14 +3,27 @@ package com.example.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WeatherDataView()
                 }
             }
         }
@@ -35,6 +48,61 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun WeatherDataView() {
+    val image: Painter = painterResource(id = R.drawable._1d)
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(id = R.string.city),
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Row(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Column() {
+                Text(
+                    text = stringResource(id = R.string.temperature),
+                    fontSize = 70.sp
+                )
+                Text(
+                    text = stringResource(id = R.string.feels_like_temp),
+                    fontSize = 15.sp
+                )
+            }
+            Spacer(modifier = Modifier.padding(horizontal = 30.dp))
+            Image(
+                painter = image,
+                contentDescription = "clear sky",
+                modifier = Modifier.size(100.dp)
+            )
+
+        }
+        Spacer(modifier = Modifier.padding(vertical = 15.dp))
+        Text(
+            text = stringResource(id = R.string.low_temp),
+            fontSize = 20.sp
+        )
+        Text(
+            text = stringResource(id = R.string.high_temp),
+            fontSize = 20.sp
+        )
+        Text(
+            text = stringResource(id = R.string.humidity),
+            fontSize = 20.sp
+        )
+        Text(
+            text = stringResource(id = R.string.pressure),
+            fontSize = 20.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
